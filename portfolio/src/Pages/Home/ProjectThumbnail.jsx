717dba70c2c53d361797f00425e0325c49b8dca1
Target <fastmovingtarget220689@gmail.com>
2025-01-06 19:36:22 +0000

    Video now plays with hover and loops

--- portfolio/src/Pages/Home/ProjectThumbnail.jsx
+++ portfolio/src/Pages/Home/ProjectThumbnail.jsx
@@ -10,7 +10,7 @@ function ProjectThumbnail ({project}) {
             <div className="tech-stack-container row">
                 {project.techStacks.map(tech => <p>{tech}</p>)}
             </div>
-            <video src={project.videoSource} alt="placeholder" onMouseOver={e => {e.target.play()}} onMouseOut={e => {e.target.pause()}} onClick={() => navigate(project.route)}/>
+            <video src={project.videoSource} alt="placeholder" onMouseOver={e => e.target.play()} onMouseOut={e => {e.target.pause()}} onClick={() => navigate(project.route)} muted loop width="500px"/>
         </div>
     )
 }
