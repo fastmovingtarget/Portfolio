345dcdcbcb789d6524ff0d45831f06bb350d59c5
Target <fastmovingtarget220689@gmail.com>
2025-04-23 16:37:01 +0100

    Minor tweaks to layout

--- portfolio/src/Pages/Layout.jsx
+++ portfolio/src/Pages/Layout.jsx
@@ -10,11 +10,6 @@ function Layout() {
     const navigate = useNavigate();
     const location = useLocation();
 
-    const navigateHandler = (path) => {
-        console.log(path)
-        navigate(path);
-    }
-
     return (
         <div className="Layout">
             <header className="layout-header">
@@ -22,17 +17,18 @@ function Layout() {
                     <img className="header-image" src={HeaderImage} alt="Target's Header"/>
                 </div>
                 <div id="header-items-container">
-                    <h1><span className="name all">James Alport, </span> <span className="qualifications mobile">MSc, BSc </span></h1>
+                    <h1 className="nameplate large">James Alport </h1>
+                    <h1 className="nameplate small">James Alport, MSc, BSc </h1>
                     <h2>Full Stack Developer</h2>
                     <h3 className="summary 1 large">I graduated with from the University of Birmingham with a Batchelors Degree in Physics and a Masters Degree in Computer Science.</h3>
                     <h3 className="summary 2 large">Since then I have worked as a Software Developer at Theorem Solutions before moving on to working on my own projects and personal growth.</h3>
-                    <h3 className="summary mobile">Software Developer, Theorem Solutions</h3>
-                    <h3 className="summary mobile">Web and Mobile App Developer and Co-Founder, Seedhack</h3>
+                    <h3 className="summary small">Software Developer, Theorem Solutions</h3>
+                    <h3 className="summary small">Web and Mobile App Developer and Co-Founder, Seedhack</h3>
                     <h3><a href={resume} download>Download Resumé ⭳</a></h3>
                 </div>
             </header>
             <div className="layout-main">
-                <NavBar navigate={navigateHandler} currentPage={location.pathname}/>
+                <NavBar navigate={navigate} currentPage={location.pathname}/>
                 <Outlet />
             </div>
         </div>
