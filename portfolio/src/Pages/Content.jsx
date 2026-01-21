ac7b304ca57151d9c2cd0fa20927990e07620517
Target <fastmovingtarget220689@gmail.com>
2026-01-21 18:15:00 +0000

    Using a wrapper to help fade in each content page

---
+++ portfolio/src/Pages/Content.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router";
+
+export default function Content() {
+
+    const location = useLocation();
+    const [currentOpacity, setCurrentOpacity] = useState(0);
+    const [targetOpacity, setTargetOpacity] = useState(1);
+
+    useEffect(() => {
+        setCurrentOpacity(0);
+        setTargetOpacity(1);
+    }, [location]);
+
+    useEffect(() => {
+        if(currentOpacity < targetOpacity) {
+            setTimeout(() => {
+                setCurrentOpacity((prev) => Math.min(prev + 0.1, targetOpacity));
+            }, 50);
+        }
+    }, [targetOpacity, currentOpacity]);
+
+    return (
+        <div style={{opacity: currentOpacity}}>
+            <Outlet/>
+        </div>
+    );
+}
