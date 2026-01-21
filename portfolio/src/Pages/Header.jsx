907161e05a30c619c5d82c61e3794796c1f76947
Target <fastmovingtarget220689@gmail.com>
2026-01-21 18:16:35 +0000

    Changed to use 2 overlaid images rather than a single one

--- portfolio/src/Pages/Header.jsx
+++ portfolio/src/Pages/Header.jsx
@@ -1,16 +1,21 @@
-import HeaderImage from "../Images/SharpPortfolio1000x1000.png"
+
+import ExperimentBlue from "../Images/ExperimentBlue.png"
+import ExperimentPurple from "../Images/ExperimentPurple.png"
 import "./Header.css"
 
 function Header() {
     return (
             <header className="layout-header">
-                <div id="header-image-container">
-                    <img className="header-image" src={HeaderImage} alt="Target's Header"/>
+                <div id="header-image-container" className="purple">
+                    <img className="header-image purple" src={ExperimentPurple} alt="Target's Header"/>
+                </div>
+                <div id="header-image-container" className="blue">
+                    <img className="header-image blue" src={ExperimentBlue} alt="Target's Header"/>
                 </div>
                 <div id="header-items-container">
                     <h1 className="nameplate large">James Alport </h1>
                     <h1 className="nameplate small">James Alport, MSc, BSc </h1>
-                    <h2>Full Stack Developer</h2>
+                    <h2 className="nameplate large">Full Stack Developer</h2>
                     <h3 className="summary 1 large">MSc Computer Science, BSc Physics</h3>
                 </div>
             </header>
